fb51edf60efb0428951c3629a6634ef58c0b833b
Hunain <hunainislam09@gmail.com>
2025-01-15 00:30:43 +0500

    pushsanity

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,13 @@
 import { createClient } from "next-sanity";
 
+import dotenv from "dotenv"
 
+dotenv.config()
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  useCdn: true, // Use CDN in production
+  useCdn: false, // Use CDN in production
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
 
